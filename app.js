edb437182c58de3b67be2c5a444a6daaf8fbb262
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2024-12-09 23:41:21 -0300

    well

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Rota para servir o template de registro
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
 //Fim tentativa
 
 
