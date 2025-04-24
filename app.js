6fd9c28f7bbb0b10e06b41f6b54132bc85b01c8a
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-04-24 14:39:17 -0300

    hotfix: returning to railway

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://auth-llibre.netlify.app",
+    origin: "https://regauthllibre-production.up.railway.app",
     credentials: true,
   })
 );
